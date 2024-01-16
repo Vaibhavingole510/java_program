2ca0bef6b884e0d181f0fdb3381ba3674b38065f
Vaibhavingole510 <155840612+Vaibhavingole510@users.noreply.github.com>
2024-01-16 12:18:36 +0530

    Add files via upload

---
+++ ifelseque.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Ifelse{
+	public static void main(String []args){
+		int age=18;
+		if(age>18){
+			System.out.println("yes boy you can come to the party"):
+		}
+		else
+		{ 
+	System.out.println("you can't come to the party")'
+		}
+	}
+}
---
+++ sum of even and odd num.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int sum=0;
+	    int n=20;
+	    for(int i=0;i<n;i++){
+	        sum=sum+(1*i);//if its 1 it will print odd and when 2 it will print even
+	    }
+	    System.out.println("sum of even numbers is:");
+	    System.out.println(sum);
+	   
+	    
+	    }
+	}
